411f6c9fda887e228a34785d1c0d869370690a6c
bustusxx <bustusxx@3a54c601-5c48-1469-4b0d-60b9207f6d9c>
2012-04-30 15:04:26 +0000

    = NPC Update/Source =
    * Fixed compilation errors due to unnamed argument in npc_unload.
    * Amended the permanent NPC variable instance system to utilise the same allocated DBMap**.
    * Fixed NPC instance variables not being passed through to callfunc; or callsub; stacks properly.
    * Removed copyarray; functionality when running set; or assigning variables.
    * Updated Novice WoE script.

--- src/map/script.c
+++ src/map/script.c
@@ -2934,6 +2934,8 @@ void pop_stack(struct script_state* st, int start, int end)
 			struct script_retinfo* ri = data->u.ri;
 			if( ri->var_function )
 				script_free_vars(ri->var_function);
+			if( data->ref )
+				aFree(data->ref);
 			aFree(ri);
 		}
 		data->type = C_NOP;
@@ -4467,10 +4469,10 @@ BUILDIN_FUNC(callfunc)
 		if( data_isreference(data) && !data->ref )
 		{
 			const char* name = reference_getname(data);
-			if( name[0] == '.' && name[1] == '@' )
-				data->ref = &st->stack->var_function;
-			else if( name[0] == '.' )
-				data->ref = &st->script->script_vars;
+			if( name[0] == '.' ) {
+				data->ref = (struct DBMap**)aCalloc(sizeof(struct DBMap*), 1);
+				data->ref[0] = (name[1] == '@' ? st->stack->var_function : st->script->script_vars);
+			}
 		}
 	}
 
@@ -4513,8 +4515,10 @@ BUILDIN_FUNC(callsub)
 		if( data_isreference(data) && !data->ref )
 		{
 			const char* name = reference_getname(data);
-			if( name[0] == '.' && name[1] == '@' )
-				data->ref = &st->stack->var_function;
+			if( name[0] == '.' && name[1] == '@' ) {
+				data->ref = (struct DBMap**)aCalloc(sizeof(struct DBMap*), 1);
+				data->ref[0] = st->stack->var_function;
+			}
 		}
 	}
 
@@ -5094,13 +5098,13 @@ BUILDIN_FUNC(set)
 {
 	TBL_PC* sd = NULL;
 	struct script_data* data;
-	struct script_data* datavalue;
+	//struct script_data* datavalue;
 	int num;
 	const char* name;
 	char prefix;
 
 	data = script_getdata(st,2);
-	datavalue = script_getdata(st,3);
+	//datavalue = script_getdata(st,3);
 	if( !data_isreference(data) )
 	{
 		ShowError("script:set: not a variable\n");
@@ -5123,6 +5127,7 @@ BUILDIN_FUNC(set)
 		}
 	}
 
+#if 0
 	if( data_isreference(datavalue) )
 	{// the value being referenced is a variable
 		const char* namevalue = reference_getname(datavalue);
@@ -5148,6 +5153,7 @@ BUILDIN_FUNC(set)
 			return buildin_copyarray(st);
 		}
 	}
+#endif
 
 	if( is_string_variable(name) )
 		set_reg(st,sd,num,name,(void*)script_getstr(st,3),script_getref(st,2));
